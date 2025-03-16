91ae438c7fd651775730e205c08a1371cce554e3
Geeta Seshapalli <124466507+geeta-seshapalli@users.noreply.github.com>
2025-03-16 10:14:01 +0530

    March 16 2025

---
+++ repairCars.java
@@ -0,0 +1,23 @@
+class Solution {
+    private boolean isPossible(int[] ranks, long mid, int cars) {
+        long carsRepaired = 0;
+        for (int rank : ranks) {
+            carsRepaired += Math.sqrt(1.0 * mid / rank);
+        }
+        return carsRepaired >= cars;
+    }
+
+    public long repairCars(int[] ranks, int cars) {
+        long l = 1, r = 1L * cars * cars * ranks[0];
+        
+        while (l < r) {
+            long mid = l + (r - l) / 2;
+            if (isPossible(ranks, mid, cars))
+                r = mid;
+            else
+                l = mid + 1;
+        }
+
+        return l;
+    }
+}
